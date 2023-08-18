bbc71fb0be0ee32ec6fb65a69ee734921b8a7cc7
muhammaddzakiardiansyah <muhammaddzakiardiansyah811@gmail.com>
2023-08-18 10:25:40 +0700

    add husky

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@ const app: Application = express();
 const port: Number = 4000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.status(200).send('halo dunia');
+    res.status(200).send({
+        code: '200',
+        message: 'success',
+        data: {
+            satu: 'satu',
+            dua: 'dua'
+        }});
 });
 
 app.listen(port, () => console.log(`app listening at port ${port}`));
